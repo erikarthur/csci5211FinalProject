d27e7f3aea3aef8a0c2be2d124eb29a2485fce8d
Erik Arthur <erik@eVM4.(none)>
2013-12-02 16:10:12 -0600

    more peer relay

--- peer/peer/PeerToPeer.cs
+++ peer/peer/PeerToPeer.cs
@@ -185,11 +185,11 @@ namespace peer
 							//need to rebroadcast msg to peers
 							if (queue==0) //server = 0, client = 1
 							{
-								c.SendCmd(msg);
+								s.SendCmd(msg);
 							}
 							else
 							{
-								s.SendCmd(msg);
+								c.SendCmd(msg);
 							}
 						}
 						
